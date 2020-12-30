16f0561816f9abeed7ba145a109810d2d2a15e08
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2020-12-29 18:31:40 -0600

    Update 8chan-x.meta.js

--- 8chan-x.meta.js
+++ 8chan-x.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Pashe's 8chanX v2 [pure]
+// @name        Pashe's 8kunX v2 [pure]
 // @version     2.0.0.1511219830
 // @description Small userscript to improve 8chan
 // @namespace   https://github.com/Pashe/8chanX/tree/2-0
