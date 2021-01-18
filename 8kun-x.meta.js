fc599c4a172047e6876531f3f1e9678c492969b7
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-01-18 16:15:36 -0600

    Update 8kun-x.meta.js

--- 8kun-x.meta.js
+++ 8kun-x.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name        8chanX for 8kun
+// @name        8KunX
 // @version     2.0.0.1511219830
 // @description Small userscript to improve 8chan
 // @namespace   https://github.com/Pashe/8chanX-for-8kun/tree/2-0
-// @updateURL   https://github.com/SlippingGitty/8chanX-for-8kun/blob/2-0_pure/8kun-x.meta.js
-// @downloadURL https://github.com/SlippingGitty/8chanX-for-8kun/blob/2-0_pure/8kun-x.user.js
+// @updateURL   https://github.com/SlippingGitty/8KunX/blob/2-0_pure/8KunX.meta.js
+// @downloadURL https://github.com/SlippingGitty/8KunX/blob/2-0_pure/8KunX.user.js
 // ==/UserScript==
