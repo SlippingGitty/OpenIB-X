d1a00a16b838571231bfb2c88ab3bdfa4a1b8cd0
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-04-04 14:13:34 -0500

    Update and rename 8kun-x.meta.js to OpenIB-X.meta.js

---
+++ OpenIB-X.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        OpenIB-X
+// @version     2021.04.04.02
+// @description An enhancement suite for OpenIB sites
+// @namespace   https://github.com/Pashe/8chanX-for-8kun/tree/2-0
+// @updateURL   https://github.com/SlippingGitty/OpenIB-X/raw/2-0_pure/OpenIB-X.meta.js
+// @downloadURL https://github.com/SlippingGitty/OpenIB-X/blob/2-0_pure/OpenIB-X.user.js
+// ==/UserScript==
