94a05faed62228115eb46782eabbf006b8d46907
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-04-04 16:34:46 -0500

    version bump

--- OpenIB-X.meta.js
+++ OpenIB-X.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        OpenIB-X
-// @version     2021.04.04.02
+// @version     2021.04.04.03
 // @description An enhancement suite for OpenIB sites
 // @namespace   https://github.com/Pashe/8chanX-for-8kun/tree/2-0
 // @updateURL   https://github.com/SlippingGitty/OpenIB-X/raw/2-0_pure/OpenIB-X.meta.js
